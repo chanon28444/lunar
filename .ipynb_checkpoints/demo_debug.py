0c78ccc8d3b4c1338b70b78e4fde076679af236d
chanon <chanon_28444@hotmail.com>
2020-07-14 14:36:22 +0700

    first commit

---
+++ .ipynb_checkpoints/demo_debug.py
@@ -0,0 +1,17 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+def rectangle(w,h):
+    return w*h
+
+area = rectangle(2,5)
+print(area)
+
+
+# %%
+
+
+
+# %%
+
+
